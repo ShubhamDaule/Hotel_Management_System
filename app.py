ab0f4601456044321e78ac20a42c2833e1447d43
unknown <shubhamdaule@gmail.com>
2025-07-02 21:16:58 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import pandas
 import date
+from django.db import models
+from django.contrib import admin
+from django.shortcuts import render, redirect
+from django.urls import path
+from django.http import HttpResponse
