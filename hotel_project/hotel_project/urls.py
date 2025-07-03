bbd6dbe31ed43a8aae1604dba2316dd66496ced9
unknown <shubhamdaule@gmail.com>
2025-07-03 00:16:57 -0600

    basic working site

--- hotel_project/hotel_project/urls.py
+++ hotel_project/hotel_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hotel.urls')),
+    path('', include('core.urls')),  # Include your app's URLs
 ]
