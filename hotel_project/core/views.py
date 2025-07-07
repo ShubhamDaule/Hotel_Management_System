f4ebf047f5bd886d7127264009f3f916378cb819
unknown <shubhamdaule@gmail.com>
2025-07-07 15:32:57 -0600

    minor update

--- hotel_project/core/views.py
+++ hotel_project/core/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Welcome to the Motel Website!")
+    print("Website is running")
+    return HttpResponse("Welcome to the Motel Website!!!!")
