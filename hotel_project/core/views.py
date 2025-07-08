8981cdf0db6ea475d5c16f200aa7173c688b7774
AishwaryaJangale <aishwaryaajangale16@gmail.com>
2025-07-07 18:38:09 -0600

    aish commits

--- hotel_project/core/views.py
+++ hotel_project/core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    print("Website is running!!!!!!!@@@@@########")
+    print("Website is running!!!!!!!@@@@@########0000")
     return HttpResponse("Welcome to the Motel Website!!!!")
