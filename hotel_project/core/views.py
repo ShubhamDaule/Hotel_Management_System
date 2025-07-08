1fa951f14ed54c70a8e0512b4d10a7590386a197
ShubhamDaule <shubhamdaule@gmail.com>
2025-07-07 18:32:09 -0600

    shubh commits

--- hotel_project/core/views.py
+++ hotel_project/core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    print("Website is running!!!!!!!")
+    print("Website is running!!!!!!!@@")
     return HttpResponse("Welcome to the Motel Website!!!!")
